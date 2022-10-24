a22871b4145a9939133ec8baa8bfb4a82e567848
smainc <40357673+smainc@users.noreply.github.com>
2022-10-24 20:37:49 +0500

    finished timeline bars

--- src/components/DisplayPanel.tsx
+++ src/components/DisplayPanel.tsx
@@ -30,15 +30,59 @@ const Title = styled.div<Props>`
     width: 100%;
     margin: 4vh 0 5vh 1vw;
 `;
+
+const TopBar = styled.div<Props>`
+    width: 100%;
+    height: 20vh;
+    background-color: ${props => props.colorTheme === 'light' ? '#fff' : props.color};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1vw;
+    margin-bottom: 1vh;
+`;
+
+const ProfileContainer = styled.div<Props>`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 20vw;
+    height: 100%;
+    background-color: ${props => props.colorTheme === 'light' ? '#fff' : props.color};
+    color: ${props => props.colorTheme === 'light' ? '#000' : '#fff'};
+    padding-right: 1vw;
+`;
+
+const Username = styled.div<Props>`
+    font-size: 1rem;
+    font-weight: 400;
+    color: ${props => props.colorTheme === 'light' ? 'rgba(0,0,0,0.5)' : 'rgba(255,255,255,0.5)'};
+    padding-right: 1vw;
+`;
+
+const Profile = styled.img<Props>`
+    width: 3.5rem;
+    height: 3.5rem;
+    border-radius: 50%;
+    background-color: #000;
+`;
 interface PropsF {
     colorTheme: string,
 }
 const DisplayPanel = ( {  colorTheme }: PropsF ) => {
     return (
         <Container colorTheme={colorTheme}>
+            <TopBar colorTheme={colorTheme}>
             <Title colorTheme={colorTheme}>
-                Welcome Back, Abuzar👋
+                Welcome Back, Ramesh👋
             </Title>
+            <ProfileContainer colorTheme={colorTheme}>
+                <Username colorTheme={colorTheme}>
+                    Ramesh Sarwal
+                </Username>
+                <Profile src="https://avatars.githubusercontent.com/u/47056243?v=4" colorTheme={colorTheme}/>
+            </ProfileContainer>
+            </TopBar>
             <Timeline colorTheme={colorTheme} />
         </Container>
     );
--- src/components/TimelineBar.tsx
+++ src/components/TimelineBar.tsx
@@ -12,13 +12,14 @@ const Container = styled.div<Props>`
     min-width: 17vw;
     max-width: 20vw;
     margin: 0 0.3vw;
+    border: ${props => props.colorTheme === 'light' ? '2px dashed rgb(28,29,34, 0.08)' : 'none'};
     border-radius: 10px;
     padding: 1vh 0;
 `;
 const Title = styled.div<Props>`
     font-size: 0.8rem;
     font-weight: 400;
-    color: ${props => props.colorTheme === 'light' ? '#000' : '#fff'};
+    color: ${props => props.colorTheme === 'light' ? 'rgba(28,29,34, 0.5)': 'rgba(255, 255, 255, 0.5)'};
     padding-left: 1vw;
     margin: 1vh 0 1vh 0.2vw;
     align-self: flex-start;
